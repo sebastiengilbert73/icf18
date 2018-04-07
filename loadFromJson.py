ad72a08571e91e4d5af93c65e67abcf1159bf84a
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2018-04-07 07:23:42 -0400

    loafFromJson.py: Skeleton for Minibatch()

--- loadFromJson.py
+++ loadFromJson.py
@@ -1,7 +1,7 @@
 # loadFromJson.py
 
 import json
-
+import torch
 
 class Importer:
     def __init__(self, filepath):
@@ -22,9 +22,18 @@ class Importer:
 
         # Create a dictionary of image_id to labels list
         self.imageIdToLabels = {}
+        self.labels = set() # Set of existing labels
         imageIdLabelsList = self.jsonObj["annotations"]
         for imageIdLabelIdsDic in imageIdLabelsList:
             image_id = int( imageIdLabelIdsDic["imageId"])
             labelsStrList = imageIdLabelIdsDic["labelId"]
             labelsList = [ int(labelStr) for labelStr in labelsStrList]
             self.imageIdToLabels[image_id] = labelsList
+            self.labels.update(labelsList)
+
+
+    def Minibatch(self, minibatchIndicesList, imageSize):
+        imagesTensor = torch.FloatTensor(len(minibatchIndicesList), 3, imageSize[1], imageSize[0])
+        targetLabelsTensor = torch.FloatTensor(len(minibatchIndicesList), len(self.labels))
+
+        return imagesTensor, targetLabelsTensor
--- loadFromJsonTester.py
+++ loadFromJsonTester.py
@@ -10,3 +10,8 @@ dataJson = importer.jsonObj
 #print ("dataJson['annotations'] =", dataJson['annotations'])
 print("importer.imageIdToUrl[1] =", importer.imageIdToUrl[1])
 print ("importer.imageIdToLabels[1] =", importer.imageIdToLabels[1])
+print ("importer.labels =", importer.labels)
+
+minibatchIndicesList = [0]
+imagesTensor, targetLabelsTensor = importer.Minibatch(minibatchIndicesList, (256, 256))
+print (imagesTensor)
