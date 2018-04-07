1e6ffad56fefde99434621f1816e77d049539ab8
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2018-04-07 06:40:11 -0400

    loadFromJson.py: Create dictionaries with image_id as key loadFromJsonTester.py: 1st commit, tester

--- loadFromJson.py
+++ loadFromJson.py
@@ -1,3 +1,30 @@
 # loadFromJson.py
 
 import json
+
+
+class Importer:
+    def __init__(self, filepath):
+        with open(filepath, 'r') as openFile:
+            fileAsString = openFile.read()
+        self.jsonObj = json.loads(fileAsString)
+        self.imageIds = []
+
+        # Create a dictionary of image_id to url
+        self.imageIdToUrl = {}
+        imageIdUrlList = self.jsonObj["images"]
+        for imageIdUrlDic in imageIdUrlList:
+            #print ("loadFromJson.Importer.__init__(): imageIdUrlDic =", imageIdUrlDic)
+            image_id = int (imageIdUrlDic["imageId"])
+            self.imageIds.append(image_id)
+            url = imageIdUrlDic["url"]
+            self.imageIdToUrl[image_id] = url
+
+        # Create a dictionary of image_id to labels list
+        self.imageIdToLabels = {}
+        imageIdLabelsList = self.jsonObj["annotations"]
+        for imageIdLabelIdsDic in imageIdLabelsList:
+            image_id = int( imageIdLabelIdsDic["imageId"])
+            labelsStrList = imageIdLabelIdsDic["labelId"]
+            labelsList = [ int(labelStr) for labelStr in labelsStrList]
+            self.imageIdToLabels[image_id] = labelsList
---
+++ loadFromJsonTester.py
@@ -0,0 +1,12 @@
+# loadFromJsonTester.py
+import json
+import loadFromJson
+
+print ("loadFromJsonTester.py")
+importer = loadFromJson.Importer('/home/sebastien/MachineLearning/BenchmarkDatasets/imaterialist-challenge-fashion-2018/train.json')
+dataJson = importer.jsonObj
+
+#print ('dataJson =', dataJson)
+#print ("dataJson['annotations'] =", dataJson['annotations'])
+print("importer.imageIdToUrl[1] =", importer.imageIdToUrl[1])
+print ("importer.imageIdToLabels[1] =", importer.imageIdToLabels[1])
