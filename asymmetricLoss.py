cc1b68838642dce8b8c9afc2ddd481c1bd667d8e
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2018-04-11 20:30:05 -0400

    assymmetricLoss.py: Fixed bug in vector size and cuda

--- asymmetricLoss.py
+++ asymmetricLoss.py
@@ -9,10 +9,10 @@ class AsymmetricL1Loss(torch.nn.Module):
         numberOfSamples = computedOutputVariable.data.shape[0]
         differenceVariable = computedOutputVariable - targetOutputVariable
         minusDifferenceVariable = -1.0 * differenceVariable
-        print ("self.penaltyForFalseNegativeVector.shape =", self.penaltyForFalseNegativeVector.shape)
-        print ("minusDifferenceVariable.data.shape =", minusDifferenceVariable.data.shape)
-        print ("differenceVariable.data.shape =", differenceVariable.data.shape)
-        lossTensorVariable = torch.autograd.Variable(self.penaltyForFalseNegativeVector.repeat(numberOfSamples)) * torch.abs(minusDifferenceVariable) + \
+        expandedPenaltyVariable = torch.autograd.Variable(self.penaltyForFalseNegativeVector.repeat(numberOfSamples, 1))
+        if minusDifferenceVariable.is_cuda:
+            expandedPenaltyVariable = expandedPenaltyVariable.cuda()
+        lossTensorVariable = expandedPenaltyVariable * torch.abs(minusDifferenceVariable) + \
                                                      1.0 * torch.abs(differenceVariable)
 
         return (1.0 / numberOfSamples) * torch.sum(lossTensorVariable)
